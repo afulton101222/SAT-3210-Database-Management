7a00d8e85ae45637e52ca1def1b894b47bda7046
Adam F <68786858+afulton101222@users.noreply.github.com>
2022-12-08 23:27:19 -0500

    Fixing broken queries project requirement 3

--- test.sql
+++ test.sql
@@ -1,2 +1,2 @@
--- SELECT SUM(quantity) AS 'Total Products' FROM product;
-SELECT COUNT(category_name) AS 'Total Types of Products' From category
+-- 6. 
+SELECT COUNT(user_id) AS 'Total Customers that Bought Coca-Cola' FROM users, orders, order_items WHERE users.user_id = orders.user_id AND orders.order_id = order_items.order_id AND Role = 'customer' AND order_items.product_id = 29 GROUP BY users.user_id;
