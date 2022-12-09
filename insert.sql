1a54a4aa244073ee93bd2109cb2837543c826873
Adam F <68786858+afulton101222@users.noreply.github.com>
2022-12-08 19:43:37 -0500

    Credentials update

--- insert.sql
+++ insert.sql
@@ -77,7 +77,6 @@ INSERT INTO Product VALUES (30, 7, 12, 'Juice', 2.99, 'Orange juice', 42);
 
 
 
-
 INSERT INTO Order_Items VALUES(1, 1, 5, 4);
 INSERT INTO Order_Items VALUES(3, 1, 3, 10);
 INSERT INTO order_items VALUES(2, 1, 50, 4);
